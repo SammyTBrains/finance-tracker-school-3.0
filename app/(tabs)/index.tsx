6c434043d67dfb86a3674cc94d9fb781c701af4f
Samuel Joseph <josephsamuel034@gmail.com>
2024-09-11 18:58:10 +0100

    hh

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,16 @@
 import { client } from "@/utils/KindeConfig";
 import services from "@/utils/services";
+import { supabase } from "@/utils/supabase";
 import { useRouter } from "expo-router";
+import { useEffect } from "react";
 import { Button, Text, View } from "react-native";
 
 export default function Home() {
   const router = useRouter();
+
+  useEffect(() => {
+    getCategoryList();
+  }, []);
 
   const handleLogout = async () => {
     const loggedOut = await client.logout();
@@ -13,6 +19,16 @@ export default function Home() {
       await services.storeData("login", "false");
       router.replace("/login");
     }
+  };
+
+  const getCategoryList = async () => {
+    const { email } = await client.getUserDetails();
+    let { data: Category, error } = await supabase
+      .from("Category")
+      .select("*")
+      .eq("created_by", email);
+
+    console.log("Data", Category);
   };
 
   return (
---
+++ utils/supabase.ts
@@ -0,0 +1,5 @@
+import { SUPABASE_KEY, SUPABASE_URL } from "@/env";
+import { createClient } from "@supabase/supabase-js";
+
+// Create a single supabase client for interacting with your database
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
