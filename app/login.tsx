dcebb1c3cc5bf265747ed7dc4f41173dec1234b0
Samuel Joseph <josephsamuel034@gmail.com>
2024-09-11 16:25:08 +0100

    chg

--- app/login.tsx
+++ app/login.tsx
@@ -1,13 +1,22 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
-const loginBg = require("./../assets/images/myApp/loginBg.jpeg");
+const loginBg = require("./../assets/images/myApp/bgmobileapp3.jpg");
 
 const LoginScreen = () => {
   return (
     <View className="flex items-center">
-      <Image source={loginBg} className="h-[400px]" />
-      <View className="bg-[#7a83e6] w-full h-full p-5 -m-[30px] rounded-tl-[20px] rounded-tr-[20px]">
-        <Text>Personal Finance and Budget Planner</Text>
+      <Image source={loginBg} className="h-[400px] w-full" />
+      <View className="bg-primary w-full h-full p-5 -m-[30px] rounded-tl-[20px] rounded-tr-[20px]">
+        <Text className="text-[35px] font-bold text-center text-white">
+          Personal Finance and Budget Planner
+        </Text>
+        <Text className="text-[18px] text-center text-white mt-5">
+          Stay on Track, Event by Event: The Best Way to Manage Your Finances
+        </Text>
+
+        <TouchableOpacity className="bg-white p-5 px-[5px] rounded-full mt-[30px]">
+          <Text className="text-center text-primary">Login/Signup</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: "#7a83e6",
+      white: "#fff",
+    },
     extend: {},
   },
   plugins: [],
