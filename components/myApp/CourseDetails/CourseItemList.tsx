0faee44630f33f995d7bda09a3adbd01ffe3cf2c
Samuel Joseph <josephsamuel034@gmail.com>
2024-09-12 03:44:08 +0100

    chg

--- components/myApp/CourseDetails/CourseItemList.tsx
+++ components/myApp/CourseDetails/CourseItemList.tsx
@@ -12,25 +12,40 @@ export default function CourseItemList(props: CourseItemListProps) {
       <Text className="font-[outfit-bold] text-xl">Item List</Text>
 
       <View className="mt-4">
-        {props.categoryData.CategoryItems.map((item: CategoryItem, index) => (
-          <View key={index} className="flex flex-row justify-between">
-            <Image
-              source={{ uri: item.image }}
-              className="w-[70px] h-[70px] rounded-2xl"
-            />
-            <View className="flex-1 ml-3">
-              <Text className="text-xl font-[outfit-bold]">{item.name}</Text>
-              <Text className="font-[outfit] text-greyDarker">{item.url}</Text>
+        {props.categoryData.CategoryItems?.length > 0 ? (
+          props.categoryData.CategoryItems.map((item: CategoryItem, index) => (
+            <View key={index}>
+              <View className="flex flex-row justify-between items-center">
+                <Image
+                  source={{ uri: item.image }}
+                  className="w-[90px] h-[90px] rounded-2xl"
+                />
+                <View className="flex-1 ml-3">
+                  <Text className="text-xl font-[outfit-bold]">
+                    {item.name}
+                  </Text>
+                  <Text className="font-[outfit] text-greyDarker">
+                    {item.url}
+                  </Text>
+                </View>
+                <Text className="text-base ml-3 font-[outfit-bold]">
+                  {item.cost.toLocaleString("en-NG", {
+                    style: "currency",
+                    currency: "NGN", // Change to Nigerian Naira
+                    minimumFractionDigits: 2, // Adjust for desired decimal places
+                  })}
+                </Text>
+              </View>
+              {props.categoryData.CategoryItems.length - 1 != index && (
+                <View className="border-[.5px] mt-3 border-grey"></View>
+              )}
             </View>
-            <Text className="text-xl font-[outfit-bold]">
-              {item.cost.toLocaleString("en-NG", {
-                style: "currency",
-                currency: "NGN", // Change to Nigerian Naira
-                minimumFractionDigits: 2, // Adjust for desired decimal places
-              })}
-            </Text>
-          </View>
-        ))}
+          ))
+        ) : (
+          <Text className="font-[outfit-bold] text-2xl text-grey">
+            No Item Found
+          </Text>
+        )}
       </View>
     </View>
   );
