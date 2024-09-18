fb3084fee76937965d371d31c5fd4893e56eb3c9
Samuel Joseph <josephsamuel034@gmail.com>
2024-09-18 16:55:59 +0100

    chg

--- app/(tabs)/ai-assistance.tsx
+++ app/(tabs)/ai-assistance.tsx
@@ -18,16 +18,32 @@ import colors from "@/components/myApp/colors";
 import { GOOGLE_API_KEY, OPENAI_API_KEY } from "@/env";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { supabase } from "@/utils/supabase";
+import { GiftedChat } from "react-native-gifted-chat";
 
 const AIAssistance = () => {
   const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY);
+  type messageType = {
+    _id: string;
+    text: string;
+    createdAt: Date;
+    user: { _id: number };
+  };
 
+  const [messages, setMessages] = useState<messageType[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [userMessage, setUserMessage] = useState("");
   const [aiResponse, setAIResponse] = useState("");
 
   const handleUserMessage = async () => {
     setIsLoading(true);
+
+    const message = {
+      _id: Math.random().toString(36).substring(7),
+      text: userMessage,
+      // createdAt: new Date(),
+      user: { _id: 1 },
+    };
+    setMessages((prevMessages) => GiftedChat.append(prevMessages, [message]));
     try {
       const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
@@ -72,17 +88,14 @@ const AIAssistance = () => {
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View className="flex-1 justify-center items-center p-5 bg-white">
-          <ScrollView
-            contentContainerStyle={{
-              flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <View>
-              <Text>{aiResponse}</Text>
-            </View>
-          </ScrollView>
+          <View className="flex-1 justify-center">
+            <Text>{aiResponse}</Text>
+            <GiftedChat
+              messages={messages}
+              renderInputToolbar={() => null}
+              user={{ _id: 1 }}
+            />
+          </View>
           <View className="p-3 border flex flex-row items-center rounded-xl border-greyDarker">
             <View className="flex flex-row items-center gap-[5px]">
               <Ionicons name="pencil" size={24} color={colors.greyDarker} />
