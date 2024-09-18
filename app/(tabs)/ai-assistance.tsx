e07bafd3f56a420396a869b007f7a45eb278c66c
Samuel Joseph <josephsamuel034@gmail.com>
2024-09-18 17:30:23 +0100

    d

--- app/(tabs)/ai-assistance.tsx
+++ app/(tabs)/ai-assistance.tsx
@@ -94,6 +94,7 @@ const AIAssistance = () => {
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View className="flex-1 p-5 bg-white">
           <View className="flex-1 justify-center">
+            <Text></Text>
             <GiftedChat
               messages={messages}
               renderInputToolbar={() => null}
